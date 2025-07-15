f5cf9c9778ad24ce50ded4de4ad375e6fed516a8
Native Earthling <lasvegas.august@live.com>
2025-07-15 11:55:56 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+basicBlockchain wallet micro: bit
+basic.forever(function() {
+    
+})
+svg.sim { box - sizing: border - box; width: 100 %; height: 100 %; display: block; } svg.sim.grayscale { -moz - filter: grayscale(1); -webkit - filter: grayscale(1); filter: grayscale(1); } .sim - button - group { cursor: pointer; } .sim - button { pointer - events: none; } .sim - board, .sim - display, sim - button { fill: #111; } .sim - button - outer: hover { stroke: grey; stroke - width: 3px; } .sim - button - nut { fill:#704A4A; pointer - events: none; } .sim - button - nut: hover { stroke: 1px solid #704A4A; } .sim - pin: hover { stroke:#D4AF37; stroke - width: 2px; } .sim - pin - touch.touched: hover { stroke: darkorange; } .sim - led - back: hover { stroke:#a0a0a0; stroke - width: 3px; } .sim - led: hover { stroke:#ff7f7f; stroke - width: 3px; } .sim - systemled { fill:#333; stroke:#555; stroke - width: 1px; } .sim - light - level - button { stroke:#fff; stroke - width: 3px; } .sim - antenna { stroke:#555; stroke - width: 2px; } .sim - text { font - family: "Lucida Console", Monaco, monospace; font - size: 25px; fill:#fff; pointer - events: none; } .sim - text - pin { font - family: "Lucida Console", Monaco, monospace; font - size: 20px; fill:#fff; pointer - events: none; } .sim - thermometer { stroke:#aaa; stroke - width: 3px; } /* animations */ .sim - flash { animation - name: sim - flash - animation; animation - duration: 0.1s; } @keyframes sim - flash - animation { from { fill: yellow; } to { fill: default ; } } .sim - flash - stroke { animation - name: sim - flash - stroke - animation; animation - duration: 0.4s; animation - timing - function: ease-in; } @keyframes sim - flash - stroke - animation { from { stroke: yellow; } to { stroke: default ; } } /* wireframe */ .sim - wireframe * { fill: none; stroke: black; }.sim - wireframe.sim - display, .sim - wireframe.sim - led, .sim - wireframe.sim - led - back, .sim - wireframe.sim - head, .sim - wireframe.sim - theme, .sim - wireframe.sim - button - group, .sim - wireframe.sim - button - label, .sim - wireframe.sim - button, .sim - wireframe.sim - text - pin { visibility: hidden; } .sim - wireframe.sim - label { stroke: none; fill: #777; } .sim - label, .sim - button - label { fill: #000; } .sim - wireframe.sim - board { stroke - width: 2px; } *: focus { outline: none; } *: focus.sim - button - outer, .sim - pin: focus, .sim - thermometer: focus, .sim - shake: focus, .sim - light - level - button: focus { stroke: #4D90FE; stroke - width: 5px!important; } .no - drag, .sim - text, .sim - text - pin { user - drag: none; user - select: none; -moz - user - select: none; -webkit - user - drag: none; -webkit - user - select: none; -ms - user - select: none; } (0, 0)(1, 0)(2, 0)(3, 0)(4, 0)(0, 1)(1, 1)(2, 1)(3, 1)(4, 1)(0, 2)(1, 2)(2, 2)(3, 2)(4, 2)(0, 3)(1, 3)(2, 3)(3, 3)(4, 3)(0, 4)(1, 4)(2, 4)(3, 4)(4, 4)
+
+P0, ANALOG INP1, ANALOG INP2, ANALOG INP3, ANALOG IN, LED Col 1P4, ANALOG IN, LED Col 2P5, BUTTON AP6, LED Col 9P7, LED Col 8P8P9, LED Col 7P10, ANALOG IN, LED Col 3P11, BUTTON BP12, RESERVED ACCESSIBILITYP13, SPI - SCKP14, SPI - MISOP15, SPI - MOSIP16, SPI - Chip SelectP17, +3v3P18, +3v3P19, I2C - SCLP20, I2C - SDAGNDGND + 3v3GND
+
+
+
+SHAKE
+svg.sim { box - sizing: border - box; width: 100 %; height: 100 %; display: block; } svg.sim.grayscale { -moz - filter: grayscale(1); -webkit - filter: grayscale(1); filter: grayscale(1); } .sim - button - group { cursor: pointer; } .sim - button { pointer - events: none; } .sim - board, .sim - display, sim - button { fill: #111; } .sim - button - outer: hover { stroke: grey; stroke - width: 3px; } .sim - button - nut { fill:#704A4A; pointer - events: none; } .sim - button - nut: hover { stroke: 1px solid #704A4A; } .sim - pin: hover { stroke:#D4AF37; stroke - width: 2px; } .sim - pin - touch.touched: hover { stroke: darkorange; } .sim - led - back: hover { stroke:#a0a0a0; stroke - width: 3px; } .sim - led: hover { stroke:#ff7f7f; stroke - width: 3px; } .sim - systemled { fill:#333; stroke:#555; stroke - width: 1px; } .sim - light - level - button { stroke:#fff; stroke - width: 3px; } .sim - antenna { stroke:#555; stroke - width: 2px; } .sim - text { font - family: "Lucida Console", Monaco, monospace; font - size: 25px; fill:#fff; pointer - events: none; } .sim - text - pin { font - family: "Lucida Console", Monaco, monospace; font - size: 20px; fill:#fff; pointer - events: none; } .sim - thermometer { stroke:#aaa; stroke - width: 3px; } /* animations */ .sim - flash { animation - name: sim - flash - animation; animation - duration: 0.1s; } @keyframes sim - flash - animation { from { fill: yellow; } to { fill: default ; } } .sim - flash - stroke { animation - name: sim - flash - stroke - animation; animation - duration: 0.4s; animation - timing - function: ease-in; } @keyframes sim - flash - stroke - animation { from { stroke: yellow; } to { stroke: default ; } } /* wireframe */ .sim - wireframe * { fill: none; stroke: black; }.sim - wireframe.sim - display, .sim - wireframe.sim - led, .sim - wireframe.sim - led - back, .sim - wireframe.sim - head, .sim - wireframe.sim - theme, .sim - wireframe.sim - button - group, .sim - wireframe.sim - button - label, .sim - wireframe.sim - button, .sim - wireframe.sim - text - pin { visibility: hidden; } .sim - wireframe.sim - label { stroke: none; fill: #777; } .sim - label, .sim - button - label { fill: #000; } .sim - wireframe.sim - board { stroke - width: 2px; } *: focus { outline: none; } *: focus.sim - button - outer, .sim - pin: focus, .sim - thermometer: focus, .sim - shake: focus, .sim - light - level - button: focus { stroke: #4D90FE; stroke - width: 5px!important; } .no - drag, .sim - text, .sim - text - pin { user - drag: none; user - select: none; -moz - user - select: none; -webkit - user - drag: none; -webkit - user - select: none; -ms - user - select: none; } (0, 0)(1, 0)(2, 0)(3, 0)(4, 0)(0, 1)(1, 1)(2, 1)(3, 1)(4, 1)(0, 2)(1, 2)(2, 2)(3, 2)(4, 2)(0, 3)(1, 3)(2, 3)(3, 3)(4, 3)(0, 4)(1, 4)(2, 4)(3, 4)(4, 4)
+
+P0, ANALOG INP1, ANALOG INP2, ANALOG INP3, ANALOG IN, LED Col 1P4, ANALOG IN, LED Col 2P5, BUTTON AP6, LED Col 9P7, LED Col 8P8P9, LED Col 7P10, ANALOG IN, LED Col 3P11, BUTTON BP12, RESERVED ACCESSIBILITYP13, SPI - SCKP14, SPI - MISOP15, SPI - MOSIP16, SPI - Chip SelectP17, +3v3P18, +3v3P19, I2C - SCLP20, I2C - SDAGNDGND + 3v3GND
+
+
+
+SHAKE
+JavaScript
+// shaking is mining...
+input.onGesture(Gesture.Shake, () => {
+    led.stopAnimation()
+    basic.clearScreen()
+    basic.pause(200) // display a short pause
+    if (Math.randomRange(0, 2) == 0) { // 30% chances to add a transaction
+        // we found a coin!!!
+        blockchain.addBlock(1);
+        basic.showIcon(IconNames.Diamond);
+    } else {
+        // missed!
+        basic.showIcon(IconNames.Asleep);
+    }
+})
+
+// show my coins
+input.onButtonPressed(Button.A, () => {
+    led.stopAnimation()
+    let coins = blockchain.valuesFrom(blockchain.id()).length;
+    basic.showNumber(coins);
+    basic.showString("COINS");
+})
+
+// show the block chain size
+input.onButtonPressed(Button.B, () => {
+    led.stopAnimation()
+    basic.showNumber(blockchain.length());
+    basic.showString("BLOCKS");
+})
+
+// instructions on how to play
+basic.showString("A=COINS B=CHAIN SHAKE=MINE").forever(function () {
+	
+})
